da0d74221d0dea9a05e61be978ccdd9e7f8278af
Emanuele Favero <emafaux89@gmail.com>
2026-05-15 00:37:42 +0200

    docs: add labels to log statements in script.js files

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,47 +1,44 @@
+// NON MODIFICARE QUESTA VARIABILE
 const teachers = ['Nathan', 'Ed', 'Fabio', 'Phil', 'Carlo', 'Lewis', 'Luca'];
+
+// Array iniziale
+console.log('Array iniziale:', teachers);
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
-console.log(fourthTeacher); // Phil
+console.log('1.', fourthTeacher); // Phil
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers[4] = 'Patrick';
-console.log(teachers[4]); // Patrick
+console.log('2.', teachers[4]); // Patrick
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
-console.log(lastTeacher); // Luca
+console.log('3.', lastTeacher); // Luca
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
-console.log(firstTeacher); // Nathan
+console.log('4.', firstTeacher); // Nathan
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push('Vanessa');
-console.log(teachers[teachers.length - 1]); // Vanessa
+console.log('5.', teachers[teachers.length - 1]); // Vanessa
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 teachers.unshift('Sarah');
-console.log(teachers[0]); // Sarah
+console.log('6.', teachers[0]); // Sarah
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
 const lewisIndex = teachers.indexOf('Lewis');
-console.log(lewisIndex); // 5
+console.log('7.', lewisIndex); // 5
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
 const isTeachersEmpty = teachers.length === 0;
-console.log(isTeachersEmpty); // false
+console.log('8.', isTeachersEmpty); // false
 
-// Log finale dell'array teachers dopo tutte le modifiche
-console.log(teachers);
-/*
-[
-  'Sarah',   'Ed',
-  'Fabio',   'Phil',
-  'Patrick', 'Lewis',
-  'Vanessa'
-]
-*/
+// Array finale
+console.log('Array finale:', teachers);
+// [ 'Sarah', 'Ed', 'Fabio', 'Phil', 'Patrick', 'Lewis', 'Vanessa' ]
--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -1,33 +1,34 @@
+// NON MODIFICARE QUESTA VARIABILE
 const teachers = ['Nathan', 'Ed', 'Fabio', 'Phil', 'Carlo', 'Lewis', 'Luca'];
+
+// Array iniziale
+console.log('Array iniziale:', teachers);
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = teachers.reverse();
-console.log(reversedTeachers);
-/*
-[
-  'Luca',   'Lewis',
-  'Carlo',  'Phil',
-  'Fabio',  'Ed',
-  'Nathan'
-]
-*/
+console.log('1.', reversedTeachers);
+// [ 'Luca', 'Lewis', 'Carlo',  'Phil', 'Fabio',  'Ed', 'Nathan' ]
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = teachers.filter((teacher) => teacher.length >= 5);
-console.log(longNames); // [ 'Lewis', 'Carlo', 'Fabio', 'Nathan' ]
+console.log('2.', longNames); // [ 'Lewis', 'Carlo', 'Fabio', 'Nathan' ]
 
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice(teachers.indexOf('Ed'), 1);
-console.log(teachers);
+console.log('3.', teachers);
 // [ 'Luca', 'Lewis', 'Carlo', 'Phil', 'Fabio', 'Nathan' ]
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = teachers.indexOf('Fabio') !== -1;
-console.log(isFabioPresent);
+console.log('4.', isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.join();
-console.log(teachersString); // Luca,Lewis,Carlo,Phil,Fabio,Nathan
+console.log('5.', teachersString); // Luca,Lewis,Carlo,Phil,Fabio,Nathan
+
+// Array finale
+console.log('Array finale:', teachers);
+// [ 'Luca', 'Lewis', 'Carlo', 'Phil', 'Fabio', 'Nathan' ]
